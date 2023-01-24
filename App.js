c6758faaf1c6f8d91298df48851d35fcbff1eca5
Junio Chaves <87779896+13Junio-Innovating@users.noreply.github.com>
2023-01-24 14:45:36 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import 'react-native-reanimated'
+import Home from './src/pagens/Home'
+
+export default function App() {
+  return (
+    <Home/>
+  );
+}
+// https://github.com/13Junio-Innovating/Financeiro
